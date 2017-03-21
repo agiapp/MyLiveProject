dc42899f41632d9d761439e3b860c71ff5229387
任波 <borenfocus@gmail.com>
2017-03-21 20:33:41 +0800

    修改生成圆角图片的方法

--- MyLiveProject/MyLiveProject/Classes/Helper/Category/UIImage+Extension.m
+++ MyLiveProject/MyLiveProject/Classes/Helper/Category/UIImage+Extension.m
@@ -12,12 +12,19 @@
 
 - (UIImage *)imageWithCornerRadius:(CGFloat)radius {
     CGRect rect = (CGRect){0.f, 0.f, self.size};
+    // 重新生成一张和目标尺寸相同的图片，参数(大小，不透明，屏幕的分辨率)
     UIGraphicsBeginImageContextWithOptions(self.size, NO, UIScreen.mainScreen.scale);
+    
     CGContextAddPath(UIGraphicsGetCurrentContext(), [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:radius].CGPath);
     CGContextClip(UIGraphicsGetCurrentContext());
+    
+    // 绘制图像
     [self drawInRect:rect];
+    // 获取绘制的图片
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    // 关闭上下文
     UIGraphicsEndImageContext();
+    
     return image;
 }
 
