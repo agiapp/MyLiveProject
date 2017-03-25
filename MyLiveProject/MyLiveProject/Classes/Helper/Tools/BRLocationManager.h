e96e53009d9a23d394fe22f45f8f4af72c7a89d2
任波 <borenfocus@gmail.com>
2017-03-25 15:16:23 +0800

    创建BRLocationManager工具类，并创建单例

---
+++ MyLiveProject/MyLiveProject/Classes/Helper/Tools/BRLocationManager.h
@@ -0,0 +1,16 @@
+//
+//  BRLocationManager.h
+//  MyLiveProject
+//
+//  Created by 任波 on 17/3/25.
+//  Copyright © 2017年 RENB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Singleton.h"
+
+@interface BRLocationManager : NSObject
+/** 声明单例 */
+singleton_interface(BRLocationManager)
+
+@end
---
+++ MyLiveProject/MyLiveProject/Classes/Helper/Tools/BRLocationManager.m
@@ -0,0 +1,15 @@
+//
+//  BRLocationManager.m
+//  MyLiveProject
+//
+//  Created by 任波 on 17/3/25.
+//  Copyright © 2017年 RENB. All rights reserved.
+//
+
+#import "BRLocationManager.h"
+
+@implementation BRLocationManager
+/** 实现单例 */
+singleton_implementation(BRLocationManager)
+
+@end
