b24e1e60124171d1da92b6202ba53327a9e460d0
任波 <borenfocus@gmail.com>
2017-04-05 23:24:47 +0800

    创建BRUserHelper

---
+++ MyLiveProject/MyLiveProject/Classes/Helper/Tools/BRUserHelper.h
@@ -0,0 +1,13 @@
+//
+//  BRUserHelper.h
+//  MyLiveProject
+//
+//  Created by 任波 on 17/4/3.
+//  Copyright © 2017年 RENB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRUserHelper : NSObject
+
+@end
---
+++ MyLiveProject/MyLiveProject/Classes/Helper/Tools/BRUserHelper.m
@@ -0,0 +1,13 @@
+//
+//  BRUserHelper.m
+//  MyLiveProject
+//
+//  Created by 任波 on 17/4/3.
+//  Copyright © 2017年 RENB. All rights reserved.
+//
+
+#import "BRUserHelper.h"
+
+@implementation BRUserHelper
+
+@end
