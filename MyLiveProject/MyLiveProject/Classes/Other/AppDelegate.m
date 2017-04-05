5740695ddf379db4b70cb71706c9170c4fef5705
任波 <borenfocus@gmail.com>
2017-04-05 23:25:23 +0800

    初始化友盟控件

--- MyLiveProject/MyLiveProject/Classes/Other/AppDelegate.m
+++ MyLiveProject/MyLiveProject/Classes/Other/AppDelegate.m
@@ -10,6 +10,9 @@
 #import "BRTabBarController.h"
 #import "BRLocationManager.h"
 #import "BRADView.h"
+#import "AppDelegate+Category.h"
+#import "UMSocial.h"
+#import "BRLoginViewController.h"
 
 @interface AppDelegate ()
 
@@ -21,7 +24,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    BRTabBarController *tabBarVC = [[BRTabBarController alloc]init];
+    BRTabBarController *tabBarVC = [[BRTabBarController alloc] init];
+    //BRLoginViewController *loginVC = [[BRLoginViewController alloc] init];
     self.window.rootViewController = tabBarVC;
     [self.window makeKeyAndVisible];
     // 广告视图
@@ -32,34 +36,18 @@
         NSLog(@"当前位置的经纬度：(%@, %@)", lat, lon);
     }];
     
+    // 初始化友盟控件
+    [self setupUMeung];
+    
     return YES;
 }
 
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    BOOL result = [UMSocialSnsService handleOpenURL:url];
+    if (result == FALSE) {
+        //调用其他SDK，例如支付宝SDK等
+    }
+    return result;
 }
 
 
